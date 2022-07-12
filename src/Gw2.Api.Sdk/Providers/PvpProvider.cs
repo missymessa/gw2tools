9f976c4e7663677d1db57a3fe9fcb8f11924843f
Missy Messa <mjanecke@microsoft.com>
2022-07-12 11:16:26 -0700

    Structuring services

---
+++ src/Gw2.Api.Sdk/Providers/PvpProvider.cs
@@ -0,0 +1,13 @@
+﻿using Gw2.Api.Sdk.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gw2.Api.Sdk.Providers
+{
+    public class PvpProvider : IPvpService
+    {
+    }
+}
